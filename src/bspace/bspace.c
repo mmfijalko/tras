968c7b19a3bf7ee69d3f4b87f504546ece575e4c
fiolek <mmfijalko@gmail.com>
2024-12-11 16:07:53 +0100

    bspace: state of the implementation

--- src/bspace/bspace.c
+++ src/bspace/bspace.c
@@ -29,7 +29,6 @@
  * SUCH DAMAGE.
  *
  * The Birthday Spacings Test.
- * The Minimum Distance Test.
  */
 
 #include <stdint.h>
@@ -49,16 +48,16 @@
 #include <stdio.h>
 
 /*
- * The birthday spacings test context.
+ * The single birthday spacings test context.
  */
-struct bspace_ctx {
+struct sbs_ctx {
 	unsigned int	m;	/* number of birthdays */
 	unsigned int	n;	/* number of days in a year */
 	unsigned int	q;	/* number of bits for a day */
 	unsigned int	b;	/* shift/bit offset for integer */
-	unsigned int	ik;	/* current number of K values */
-	unsigned int *	K;	/* ??? */
-	double *	pprob;	/* Poisson theoretical probabilities */
+//	unsigned int	ik;	/* current number of K values */
+//	unsigned int *	K;	/* ??? */
+//	double *	pprob;	/* Poisson theoretical probabilities */
 	unsigned int *	bdays;	/* birthdays list for single K */
 	unsigned int *	intvs;	/* intervals table */
 	unsigned int	nbits;	/* number of bits processed */
@@ -115,14 +114,14 @@ quicksort(unsigned int *table, int n)
 }
 
 inline static void
-bspace_sort_bdays(struct bspace_ctx *c)
+sbs_sort_bdays(struct sbs_ctx *c)
 {
 
 	quicksort(c->bdays, c->m);
 }
 
 inline static void
-bspace_sort_intvs(struct bspace_ctx *c)
+sbs_sort_intvs(struct sbs_ctx *c)
 {
 
 	quicksort(c->intvs, c->m);
@@ -144,7 +143,7 @@ rotate32(uint32_t u, int off)
 }
 
 static inline uint32_t
-bspace_be32enc(void *d)
+sbs_be32enc(void *d)
 {
 	uint8_t *p = (uint8_t *)d;
 
@@ -155,41 +154,51 @@ bspace_be32enc(void *d)
  * TODO: To implement. Temporary take 24 LSb.
  */
 static inline uint32_t
-bspace_extract32(void *d, int o, int q)
+sbs_extract32(void *d, int o, int q)
 {
 
-	return (bspace_be32enc(d) & 0x00ffffff);
+	return (sbs_be32enc(d) & 0x00ffffff);
 }
 
-static double
-bspace_poison_pdf(int k, double lambda);
-
-int
-bspace_init(struct tras_ctx *ctx, void *params)
+static int
+bspace_verify_params(struct bspace_params *p, int single)
 {
-	struct bspace_params *p = params;
-	struct bspace_ctx *c;
-	size_t size;
-	int error;
-
-	TRAS_CHECK_INIT(ctx);
-	TRAS_CHECK_PARA(p, p->alpha);
 
 	if ((p->q == 0) || (p->q > 31) || (p->n != (1 << p->q)))
 		return (EINVAL);
 	if (p->b < BSPACE_MIN_BIT_OFFSET || p->b > BSPACE_MAX_BIT_OFFSET)
 		return (EINVAL);
 	if (p->m == 0)
+	       return (EINVAL);
+	if (single && p->jn != 1)
 		return (EINVAL);
-	if (p->jn < BSPACE_MIN_JN || p->jn > BSPACE_MAX_JN)
+	if (!single && ((p->jn < BSPACE_MIN_JN || p->jn > BSPACE_MAX_JN)))
 		return (EINVAL);
 
 	/* todo: check m, n */
 
-	size = sizeof(struct bspace_ctx) + p->m * sizeof(uint32_t) +
+	return (0);
+}
+
+int
+sbs_init(struct tras_ctx *ctx, void *params)
+{
+	struct bspace_params *p = params;
+	struct sbs_ctx *c;
+	size_t size;
+	int error;
+
+	TRAS_CHECK_INIT(ctx);
+	TRAS_CHECK_PARA(p, p->alpha);
+
+	error = bspace_verify_params(p, 1);
+	if (error != 0)
+		return (error);
+
+	size = sizeof(struct sbs_ctx) + p->m * sizeof(uint32_t) +
 	    p->m * sizeof(uint32_t);
 
-	error = tras_init_context(ctx, &bspace_algo, size, TRAS_F_ZERO);
+	error = tras_init_context(ctx, &sbs_algo, size, TRAS_F_ZERO);
 	if (error != 0)
 		return (error);
 
@@ -211,9 +220,9 @@ bspace_init(struct tras_ctx *ctx, void *params)
 }
 
 int
-bspace_update(struct tras_ctx *ctx, void *data, unsigned int nbits)
+sbs_update(struct tras_ctx *ctx, void *data, unsigned int nbits)
 {
-	struct bspace_ctx *c;
+	struct sbs_ctx *c;
 	unsigned int n, i, b;
 	uint32_t *p;
 
@@ -231,23 +240,23 @@ bspace_update(struct tras_ctx *ctx, void *data, unsigned int nbits)
 	n = n + b;
 
 	for (i = b; i < n; i++, p++)
-		c->bdays[i] = bspace_extract32(p, c->b, c->q);
+		c->bdays[i] = sbs_extract32(p, c->b, c->q);
 	c->nbits += nbits;
 
 	return (0);
 }
 
 static double
-bspace_poison_pdf(int k, double lambda)
+sbs_poison_pdf(int k, double lambda)
 {
 
 	return (exp(-lambda) * pow(lambda, k) / tgamma(k + 1));
 }
 
 int
-bspace_final(struct tras_ctx *ctx)
+sbs_final(struct tras_ctx *ctx)
 {
-	struct bspace_ctx *c;
+	struct sbs_ctx *c;
 	double pvalue, sobs, lambda;
 	unsigned int i, K, diff;
 	int sum;
@@ -262,7 +271,11 @@ bspace_final(struct tras_ctx *ctx)
 	/*
 	 * Sort generated birthdays, let them be positioned as in calendar.
 	 */
-	bspace_sort_bdays(c);
+	sbs_sort_bdays(c);
+
+	for (i = 0; i < c->m; i++)
+		printf("b[%d] = %u\n", i, c->bdays[i]);
+	printf("\n");
 
 	/*
 	 * Create intervals.
@@ -271,10 +284,18 @@ bspace_final(struct tras_ctx *ctx)
 	for (i = 1; i < c->m; i++)
 		c->intvs[i] = c->bdays[i] - c->bdays[i - 1];
 
+	for (i = 0; i < c->m; i++)
+		printf("intv[%d] = %u\n", i, c->intvs[i]);
+	printf("\n");
+
 	/*
 	 * Sort intervals list.
 	 */
-	bspace_sort_intvs(c);
+	sbs_sort_intvs(c);
+
+	for (i = 0; i < c->m; i++)
+		printf("intv[%d] = %u\n", i, c->intvs[i]);
+	printf("\n");
 
 	/*
 	 * Count spacing values between birthdays that occurs more than once.
@@ -308,7 +329,161 @@ bspace_final(struct tras_ctx *ctx)
 	else
 		ctx->result.status = TRAS_TEST_PASSED;
 
-	ctx->result.discard = 0; /* c->nbits - c->nk * c->m * c->q; ??? */
+	ctx->result.discard = c->nbits - 32 * c->m;
+	ctx->result.pvalue1 = pvalue;
+	ctx->result.pvalue2 = 0.0;
+	ctx->result.stats1 = (double)K;
+	ctx->result.stats2 = 0.0;
+
+	tras_fini_context(ctx, 0);
+
+	return (0);
+}
+
+int
+sbs_test(struct tras_ctx *ctx, void *data, unsigned int nbits)
+{
+
+	return (tras_do_test(ctx, data, nbits));
+}
+
+int
+sbs_restart(struct tras_ctx *ctx, void *params)
+{
+
+	return (tras_do_restart(ctx, params));
+}
+
+int
+sbs_free(struct tras_ctx *ctx)
+{
+
+	return (tras_do_free(ctx));
+}
+
+const struct tras_algo sbs_algo = {
+	.name =		"births",
+	.desc =		"The single Birthday Spacing Test",
+	.id =		0,
+	.version =	{ 0, 1, 1 },
+	.init =		sbs_init,
+	.update =	sbs_update,
+	.test =		sbs_test,
+	.final =	sbs_final,
+	.restart =	sbs_restart,
+	.free =		sbs_free,
+};
+
+/*
+ * The first level birthday spacing test.
+ */
+struct bspace_ctx {
+	unsigned int		jidx;	/* index of current statistics */
+	unsigned int		jmax;	/* maximum number of statistics */
+	unsigned int *		jtab;	/* table of K statistics */
+	unsigned int		nbits;	/* number of bits updated */
+	double			alpha;	/* significance level for H0 */
+	struct sbs_ctx		bsctx;	/* context for single birtdady test */
+	struct tras_ctx		sbctx;	/* tras context for single test */
+	struct bspace_params	param;	/* the single test params */
+};
+
+int
+bspace_init(struct tras_ctx *ctx, void *params)
+{
+	struct bspace_params *p = params;
+	struct bspace_ctx *c;
+	size_t size;
+	int error;
+
+	TRAS_CHECK_INIT(ctx);
+	TRAS_CHECK_PARA(p, p->alpha);
+
+	error = bspace_verify_params(p, 0);
+	if (error != 0)
+		return (error);
+
+	size = sizeof(struct bspace_ctx) + p->jn * sizeof(unsigned int);
+
+	error = tras_init_context(ctx, &bspace_algo, size, TRAS_F_ZERO);
+	if (error != 0)
+		return (error);
+
+	c = ctx->context;
+
+	c->param.m = p->m;
+	c->param.n = p->n;
+	c->param.b = p->b;
+	c->param.q = p->q;
+	c->param.jn = 1;
+	c->param.alpha = p->alpha;
+
+	c->jtab = (unsigned int *)(c + 1);
+	c->jmax = p->jn;
+	c->jidx = 0;
+	c->alpha = p->alpha;
+
+	return (0);
+}
+
+int
+bspace_update(struct tras_ctx *ctx, void *data, unsigned int nbits)
+{
+	struct bspace_ctx *c;
+	struct sbs_ctx *sbc;
+	unsigned int i, k, sn;
+
+	TRAS_CHECK_UPDATE(ctx, data, nbits);
+
+	c = ctx->context
+
+	if (nbits & 0x1f)
+		return (EINVAL);
+
+	/* The number of words updated */
+	b = c->nbits / 32;
+
+	/* How many single statistics to update */
+	n = miss(c->jidx, c->jmax);
+
+	/* The number of words for single birthday spacing test */
+	sn = 
+
+	n = min(n, nbits / 32);
+	n = n + b;
+
+	if (c->jidx >= c->max) {
+		c->nbits += nbits;
+		return (0);
+	}
+
+	ctx->nbits += nbits;
+
+	return (0);
+}
+
+int
+bspace_final(struct tras_ctx *ctx)
+{
+	struct bspace_ctx *c;
+	double pvalue;
+
+	TRAS_CHECK_FINAL(ctx);
+
+	c = ctx->context;
+
+	if (c->jidx < c->jmax)
+		return (EALREADY);
+
+	/* XXX: temporary */
+	pvalue = 0.0;
+
+	if (pvalue < c->alpha)
+		ctx->result.status = TRAS_TEST_FAILED;
+	else
+		ctx->result.status = TRAS_TEST_PASSED;
+
+	ctx->result.discard = 0; /* ??? */
 	ctx->result.pvalue1 = pvalue;
 
 	tras_fini_context(ctx, 0);
@@ -349,3 +524,5 @@ const struct tras_algo bspace_algo = {
 	.restart =	bspace_restart,
 	.free =		bspace_free,
 };
+
+
