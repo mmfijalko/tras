85e9b5ffba11c51109db0ac672cf996e6f58b24a
marcin <mmfijalko@gmail.com>
2025-05-18 14:13:17 +0200

    operm5: functions to generate index of permuation and permutation from index, tested, working well for n=5

--- src/operm5/permindex.c
+++ src/operm5/permindex.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-
 static int
 operm5_perm_to_index(uint32_t *perm, int n)
 {
@@ -21,44 +20,80 @@ operm5_perm_to_index(uint32_t *perm, int n)
 }
 
 static void
-operm5_index_to_perm(uint32_t *perm, int n, uint32_t index)
+operm5_index_to_perm(uint32_t index, uint32_t *perm, int n)
 {
-	uint32_t fac, div;
+	uint32_t fac, val;
+	int i, j, idx, div;
+
+	for (i = 0, fac = 1; i < n; i++) {
+		if (i > 0)
+			fac = fac * i;
+		perm[i] = i + 1;
+	}
+
+	for (i = 0; i < n; i++) {
+		idx = index / fac;
+		index = index - idx * fac;
+		div = (n - 1 - i);
+		div = (div != 0) ? div : 1;
+		fac = fac / div;
+		val = perm[i + idx];
+		for (j = i + idx; j > i; j--)
+			perm[j] = perm[j - 1];
+		perm[i] = val;
+	}
+}
+
+static void
+operm5_int_to_perm(uint32_t *perm, int n, uint32_t value)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--) {
+		perm[i] = value % 10;
+		value = value / 10;
+	}	
+}
+
+static int
+operm5_perm_valid(uint32_t *perm, int n)
+{
 	int i, j;
 
-	for (i = 1, fac = 1; i < n; i++)
-		fac = fac * i;
-
-	div = n - 2;
 	for (i = 0; i < n; i++) {
-		j = index / fac;
-		printf("j = %d\n", j);
-		index = index - j * fac;
-		printf("index = %d\n", index);
-		if (j == 0)
-			perm[i] = i;
-		else
-			perm[i] = j;
-		fac = fac / ((div == 0) ? 1 : div);
-		div--;
+		if (perm[i] < 1 || perm[i] > n)
+			return (0);
+		for (j = i + 1; j < n; j++) {
+			if (perm[i] == perm[j])
+				return (0);
+		}
 	}
+	return (1);
 }
 
 int main(void)
 {
-	uint32_t perm[5] = {1, 2, 3, 4, 5}, index;
+	uint32_t perm[5], index, index2;
 	uint32_t idx, n = 5, i;
 
-	for (idx = 0; idx < 120; idx++) {
-		operm5_index_to_perm(perm, 5, idx);
-		index = operm5_perm_to_index(perm, 5);
-		printf("(");
-		for (i = 0; i < 5; i++) {
-			if (i != 0)
-				printf(", ");
-			printf("%d", perm[i]);
+	for (i = 1; i < 55556; i++) {
+		operm5_int_to_perm(perm, n, i);
+		if (!operm5_perm_valid(perm, n))
+			continue;
+		printf("permutation := [");
+		for (idx = 0; idx < n; idx++)
+			printf("%d%s", perm[idx], (idx == (n - 1)) ? "], " : " ");
+		index = operm5_perm_to_index(perm, n);
+		printf("index = %d, ", index);
+		operm5_index_to_perm(index, perm, n);
+		printf("reverted permutation := [");
+		for (idx = 0; idx < n; idx++)
+			printf("%d%s", perm[idx], (idx == (n - 1)) ? "]\n, " : " ");
+		index2 = operm5_perm_to_index(perm, n);
+		if (index != index2) {
+			printf("failed with permutation, index = %d, index2 = %d\n", index, index2);
+			break;
 		}
-		printf("), index = %d\n", index);
 	}
 
 	return (0);
